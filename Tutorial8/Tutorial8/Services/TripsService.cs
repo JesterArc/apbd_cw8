79a41eefe944ac27433d9f61408643a9edb9f686
JesterArc <125870978+JesterArc@users.noreply.github.com>
2025-05-10 00:07:24 +0200

    added comments to TripsService

--- Tutorial8/Tutorial8/Services/TripsService.cs
+++ Tutorial8/Tutorial8/Services/TripsService.cs
@@ -12,6 +12,7 @@ public class TripsService : ITripsService
     public async Task<List<TripDTO>> GetTrips()
     {
         var trips = new List<TripDTO>();
+        // this will return all info about a trip + the country it takes place in
         string command = """
                          SELECT t.IdTrip, t.Name, t.Description, t.DateFrom, t.DateTo, t.MaxPeople, c.Name FROM Trip t 
                          INNER JOIN Country_Trip ct ON t.IdTrip = ct.IdTrip 
@@ -29,6 +30,7 @@ public class TripsService : ITripsService
                 {
                     var id = reader.GetInt32(0);
                     var trip = trips.FirstOrDefault(e => e.Id.Equals(id));
+                    // if this trip is not already in our list of trips
                     if (trip == null)
                     {
                         trip = new TripDTO()
@@ -45,6 +47,7 @@ public class TripsService : ITripsService
                     }
                     String name = reader.GetString(6);
                     var country = trip.Countries.FirstOrDefault(e => e.Name.Equals(name));
+                    // if this country is not already in our list of countries for the trip
                     if (country is null)
                     {
                         country = new CountryDTO()
@@ -63,6 +66,7 @@ public class TripsService : ITripsService
     public async Task<TripDTO> GetTripById(int id)
     {
         TripDTO? trip = null;
+        // returns all info about trip specified by id plus name of country it takes place in
         var command = """
                       SELECT t.IdTrip, t.Name, t.Description, t.DateFrom, t.DateTo, t.MaxPeople, c.Name FROM Trip t 
                       INNER JOIN Country_Trip ct ON t.IdTrip = ct.IdTrip 
@@ -79,6 +83,8 @@ public class TripsService : ITripsService
                 
                 while (await reader.ReadAsync())
                 {
+                    // there is a validator method called before this one so the trip can only be null if
+                    // this is the first iteration
                     if (trip == null)
                     {
                         trip = new TripDTO()
@@ -94,6 +100,7 @@ public class TripsService : ITripsService
                     }
                     String name = reader.GetString(6);
                     var country = trip.Countries.FirstOrDefault(e => e.Name.Equals(name));
+                    // if this country is not already in our list of countries for the trip
                     if (country is null)
                     {
                         country = new CountryDTO()
@@ -105,7 +112,7 @@ public class TripsService : ITripsService
                 }
             }
         }
-
+        // realistically cannot happen, but I must add it or compiler will complain
         if (trip == null)
         {
             return new TripDTO();
@@ -115,7 +122,9 @@ public class TripsService : ITripsService
 
     public async Task<bool> DoesTripExist(int id)
     {
+        // returns if this id is tied to a trip in the Trip table
         var quantity = 0;
+        // counts how many times it appears
         var command = "Select Count(1) from Trip where IdTrip = @IdTrip";
         using (SqlConnection conn = new SqlConnection(_connectionString))
         using (SqlCommand cmd = new SqlCommand(command, conn))
@@ -130,12 +139,14 @@ public class TripsService : ITripsService
                 }
             }
         }
+        // does it appear at least once
         return quantity > 0;
     }
 
     public async Task<bool> CanTripFitOneMore(int tripId)
     {
         var quantity = 0;
+        // returns how many slots are left on this trip
         var command = @"Select (t.MaxPeople - Count(1)) from dbo.Client_Trip ct
                         Join dbo.Trip T on ct.IdTrip = T.IdTrip
                         where ct.IdTrip = @tripId
@@ -153,6 +164,7 @@ public class TripsService : ITripsService
                 }
             }
         }
+        // returns if there are any seats remaining
         return quantity > 0;
     }
 }
