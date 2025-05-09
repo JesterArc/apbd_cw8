63454d140ddb8ccf81e1f5d04a2d84a343b97bc1
JesterArc <125870978+JesterArc@users.noreply.github.com>
2025-05-09 17:04:04 +0200

    1. GET /api/trips complete

--- Tutorial8/Tutorial8/Services/TripsService.cs
+++ Tutorial8/Tutorial8/Services/TripsService.cs
@@ -12,8 +12,11 @@ public class TripsService : ITripsService
     public async Task<List<TripDTO>> GetTrips()
     {
         var trips = new List<TripDTO>();
-
-        string command = "SELECT IdTrip, Name FROM Trip";
+        string command = """
+                         SELECT t.IdTrip, t.Name, t.Description, t.DateFrom, t.DateTo, t.MaxPeople, c.Name FROM Trip t 
+                         INNER JOIN Country_Trip ct ON t.IdTrip = ct.IdTrip 
+                         INNER JOIN Country c ON ct.IdCountry = c.IdCountry
+                         """;
         
         using (SqlConnection conn = new SqlConnection(_connectionString))
         using (SqlCommand cmd = new SqlCommand(command, conn))
@@ -24,14 +27,35 @@ public class TripsService : ITripsService
             {
                 while (await reader.ReadAsync())
                 {
-                    int idOrdinal = reader.GetOrdinal("IdTrip");
-                    trips.Add(new TripDTO()
+                    var id = reader.GetInt32(0);
+                    var trip = trips.FirstOrDefault(e => e.Id.Equals(id));
+                    if (trip == null)
                     {
-                        Id = reader.GetInt32(idOrdinal),
-                        Name = reader.GetString(1),
-                    });
+                        trip = new TripDTO()
+                        {
+                            Id = id,
+                            Name = reader.GetString(1),
+                            Description = reader.GetString(2),
+                            DateFrom = reader.GetDateTime(3),
+                            DateTo = reader.GetDateTime(4),
+                            MaxPeople = reader.GetInt32(5),
+                            Countries = new List<CountryDTO>(),
+                        };
+                        trips.Add(trip);
+                    }
+                    String name = reader.GetString(6);
+                    var country = trip.Countries.FirstOrDefault(e => e.Name.Equals(name));
+                    if (country is null)
+                    {
+                        country = new CountryDTO()
+                        {
+                            Name = name
+                        };
+                        trip.Countries.Add(country);
+                    }
                 }
             }
+            
         }
         return trips;
     }
